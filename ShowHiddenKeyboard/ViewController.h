bd9d4d216e32ec09a77b535e767fab8fe656ed15
陈宇峰 <yufengcode@gmail.com>
2016-06-04 09:59:30 +0800

    Initial Commit

---
+++ ShowHiddenKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowHiddenKeyboard
+//
+//  Created by chenyufeng on 16/6/4.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShowHiddenKeyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShowHiddenKeyboard
+//
+//  Created by chenyufeng on 16/6/4.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

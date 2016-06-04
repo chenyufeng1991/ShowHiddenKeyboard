0fada4df32a6683bb411a7c5bc40f1437b9d5683
陈宇峰 <yufengcode@gmail.com>
2016-06-04 10:05:53 +0800

    init

---
+++ ShowHiddenKeyboard/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShowHiddenKeyboard
+//
+//  Created by chenyufeng on 16/6/4.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"主界面";
+
+
+    
+}
+
+
+@end
